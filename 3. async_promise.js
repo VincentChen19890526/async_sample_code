198bdf03e9054155b1b76f15091cd33e8baa1c19
ivanchiou <wyattkidd@gmail.com>
2023-04-22 17:36:16 +0800

    fixed promise await async process

--- 3. async_promise.js
+++ 3. async_promise.js
@@ -3,7 +3,7 @@ const { users, orders }  = require('../data')
 // promise是ES6提供來解決callback function造成callback hell的方法，
 // 被包覆在promise內的function，會保證在呼叫resolve後才會執行.then裡面的程序，
 // 避免callback會一直往下層寫，造成不好閱讀的程式碼
-function orders_by_user(resolve, user_index) {
+users.forEach((user)=>{
     //確認每個user即將點餐
     return new Promise((resolve, _reject)=>{
         orders.forEach((order)=>{
@@ -18,13 +18,6 @@ function orders_by_user(resolve, user_index) {
             }
         })
     }).then(()=>{
-        //確認所有users都下訂完成
-    })
-}
 
-new Promise((resolve, _reject)=>{
-    return orders_by_user(resolve, user_index)
-}).then(()=>{
-    console.log("所有料理結束")
-    process.exit()
+    })
 })
