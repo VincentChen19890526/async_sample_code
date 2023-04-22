cad622841793d28a7a7fdcb301942e2919e64a6b
ivanchiou <wyattkidd@gmail.com>
2023-04-22 17:28:08 +0800

    fixed promise await async process

--- 3. async_promise.js
+++ 3. async_promise.js
@@ -1,11 +1,9 @@
-const { users, orders }  = require('./data')
-let user_index = 0
+const { users, orders }  = require('../data')
 
 // promise是ES6提供來解決callback function造成callback hell的方法，
 // 被包覆在promise內的function，會保證在呼叫resolve後才會執行.then裡面的程序，
 // 避免callback會一直往下層寫，造成不好閱讀的程式碼
 function orders_by_user(resolve, user_index) {
-    let user = users[user_index]
     //確認每個user即將點餐
     return new Promise((resolve, _reject)=>{
         orders.forEach((order)=>{
@@ -13,7 +11,7 @@ function orders_by_user(resolve, user_index) {
             if (order.category === user.favorite) {
                 //開始餐點下單
                 new Promise((resolve, _reject)=>{
-                    //餐點完成
+
                 }).then(()=>{
                     //確定餐點完成
                 })
--- 4. async_await.js
+++ 4. async_await.js
@@ -1,9 +1,8 @@
 const { users, orders }  = require('../data')
-let user_index = 0
+
 // async await是ES7提供來解決promise閱讀起來還是並非按照從上而下的順序，而包裝promise後設計出來的功能
 // async await可配合promise使用，但必須注意不能混用到.then
-async function orders_by_user(user_index) {
-    let user = users[user_index]
+users.forEach(async (user)=>{
     //確認每個user即將點餐
     await new Promise((resolve, _reject)=>{
         orders.forEach(async (order)=>{
@@ -11,13 +10,11 @@ async function orders_by_user(user_index) {
             if (order.category === user.favorite) {
                 //開始餐點下單
                 await setTimeout(()=>{
-                    //確定餐點完成 
+                    //確定餐點完成
                 }, order.cooking_time * 100)
             }
         })
     })
-    user_index ++
-    //確認所有users都下訂完成
-}
 
-orders_by_user(user_index)
+    //確認所有users都下訂完成
+})
