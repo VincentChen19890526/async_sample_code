82bbc62c2e93d0eb2d3ec84ad8e2320f54585b3f
VincentChen19890526 <vincent780526@gmail.com>
2024-05-26 11:31:21 +0800

    練習修改後的版本

--- 2. async_callback.js
+++ 2. async_callback.js
@@ -1,14 +1,17 @@
-const { users, orders }  = require('./data')
+const { users, orders } = require('./data')
 
 // callback function是代替設定等待時間的function，server完成後會給予一個ack，告知client完成
-users.forEach((user)=>{
+users.forEach((user) => {
     //確認每個user即將點餐
-    orders.forEach((order)=>{
+    console.log("客人" + user.name + "-點餐")
+    orders.forEach((order) => {
         //對每個user建立要下訂的餐點
         if (order.category === user.favorite) {
             //開始餐點下單
-            setTimeout(()=>{
+            console.log("下單-" + order.name + ":開始烹飪")
+            setTimeout(() => {
                 //確定餐點完成
+                console.log(user.name + ":" + order.name + "-料理完成")
             }, order.cooking_time * 100)
         }
     })
--- 3. async_promise.js
+++ 3. async_promise.js
@@ -1,23 +1,40 @@
-const { users, orders }  = require('../data')
+const { users, orders } = require('./data')
+let user_complete = 0
 
 // promise是ES6提供來解決callback function造成callback hell的方法，
 // 被包覆在promise內的function，會保證在呼叫resolve後才會執行.then裡面的程序，
 // 避免callback會一直往下層寫，造成不好閱讀的程式碼
-users.forEach((user)=>{
+users.forEach((user) => {
     //確認每個user即將點餐
-    return new Promise((resolve, _reject)=>{
-        orders.forEach((order)=>{
+    console.log("幫忙" + user.name + "點餐")
+    return new Promise((resolve, _reject) => {
+        let done = 0
+        orders.forEach((order) => {
             //對每個user建立要下訂的餐點
             if (order.category === user.favorite) {
                 //開始餐點下單
-                new Promise((resolve, _reject)=>{
-
-                }).then(()=>{
+                console.log("開始烹飪" + user.name + "的餐點" + order.name)
+                new Promise((resolve, _reject) => {
+                    setTimeout(resolve, order.cooking_time * 100)
+                }).then(() => {
                     //確定餐點完成
+                    console.log(user.name + "的" + order.name + "烹飪好了")
+                    done += 1
+                    if (done === 3) {
+                        resolve()
+                    }
                 })
             }
         })
-    }).then(()=>{
-
+    }).then(() => {
+        user_complete++
+        if (user_complete === users.length) {
+            console.log("所有客人的料理都完成了")
+            process.exit()
+        }
     })
 })
+
+
+
+
